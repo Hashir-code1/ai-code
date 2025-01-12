9b8142fc43ed3a1362ec7982cdd02256155aa78e
Hashir-code1 <ahmadhashir222@gmail.com>
2025-01-12 12:17:11 +0000

    day1 code is done

---
+++ day1/class/ac1.py
@@ -0,0 +1,13 @@
+print("Hello")
+a=input("what is your name?")
+print(a)
+print("how was your day?")
+b=input("Was it good or bad?")
+if b=="good":
+    print("Thats good to hear")
+elif b=="bad":
+    print("It will get better")
+else:
+    print("I didnt understand")
+
+print(f"It was nice talking to you {a}")
